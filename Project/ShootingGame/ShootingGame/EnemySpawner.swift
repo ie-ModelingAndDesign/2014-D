a30ce04035a937af703d97d18ded360309723815
Daiki OYAKAWA <daiki.evilone@gmail.com>
2015-02-21 02:39:08 +0900

    EnemyManagerにコメントをつけた。

--- Project/ShootingGame/ShootingGame/EnemySpawner.swift
+++ Project/ShootingGame/ShootingGame/EnemySpawner.swift
@@ -21,19 +21,19 @@ class EnemySpawner{
     
     func update(){
         timeA++;
-        if(timeA >= 60){
+        if(timeA >= 30){
             timeA = 0;
             spawnEnemy(0);
         }
         timeB++;
         if(timeB >= 120){
             timeB = 0;
-            spawnEnemy(1);
+            spawnEnemy(2);
         }
         timeC++;
-        if(timeC >= 60){
+        if(timeC >= 240){
             timeC = 0;
-            spawnEnemy(2);
+            spawnEnemy(3);
         }
     }
     
@@ -41,36 +41,44 @@ class EnemySpawner{
         var enemy:Enemy!
         switch(n){
         case 0:
+            // Sin波でX軸をウロウロしながら降りてくる
             enemy = DaikiEnemy(obj: myscene)
             enemy.colliderRadius = 40
             enemy.setTexture("daiki2")
             break;
         case 1:
+            // 左右に往復
             enemy = MoriEnemy(obj:myscene)
             enemy.colliderRadius = 60
             enemy.setTexture("moriyuki1")
             enemy.HP = 10.0
         case 2:
+            // ランダム移動
             enemy = MoriEnemy2(obj:myscene)
             enemy.colliderRadius = 60
-            enemy.setTexture("daiki1")
+            enemy.setTexture("koki_1")
         case 3:
+            // 横から飛び出る
             enemy = MoriEnemy3(obj:myscene)
             enemy.colliderRadius = 60
             enemy.setTexture("daiki1")
         case 4:
+            // 上から落ちてくる
             enemy = MoriEnemy5(obj:myscene)
             enemy.colliderRadius = 60
             enemy.setTexture("akihiko1")
         case 5:
+            // 回る
             enemy = MoriEnemy6(obj:myscene)
             enemy.colliderRadius = 60
             enemy.setTexture("akihiko2")
         case 6:
+            // 楕円
             enemy = MoriEnemy7(obj:myscene)
             enemy.colliderRadius = 60
             enemy.setTexture("akihiko3")
         case 7:
+            // 不規則にカーブ
             enemy = MoriEnemy9(obj:myscene)
             enemy.colliderRadius = 60
             enemy.setTexture("akihiko4")
