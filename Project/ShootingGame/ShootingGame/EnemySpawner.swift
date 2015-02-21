c5f5c969d52bf6cecf0e573974fefae1332682dc
Daiki OYAKAWA <daiki.evilone@gmail.com>
2015-02-21 17:32:48 +0900

    MoriEnemy2のランダム範囲を広くした。

--- Project/ShootingGame/ShootingGame/EnemySpawner.swift
+++ Project/ShootingGame/ShootingGame/EnemySpawner.swift
@@ -23,7 +23,7 @@ class EnemySpawner{
         timeA++;
         if(timeA >= 30){
             timeA = 0;
-            spawnEnemy(1);
+            spawnEnemy(2);
         }
         timeB++;
         if(timeB >= 120){
